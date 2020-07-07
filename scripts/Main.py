2d5ddff4242fc5e4854bbf7c90fd6c3a17ff2979
furu8 <foooolurk1125@gmail.com>
2020-07-07 20:22:55 +0900

    main

---
+++ scripts/Main.py
@@ -0,0 +1,15 @@
+from pixivpy3 import *
+import json
+import pandas as pd
+from UserRecord import UserRecord
+from DownloadIllust import DownloadIllust
+
+if __name__ == "__main__":
+    ur = UserRecord()
+    dl = DownloadIllust()
+
+    # ユーザ情報を保存
+    ur.post_user_record('../info/follow_user_account/user_account.json')
+
+    # ユーザのイラストを保存
+    dl.download()
