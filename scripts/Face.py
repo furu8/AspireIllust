c7bf0d013f928ec0f6a3a18a9a51cbe2e19c8b6f
furu8 <foooolurk1125@gmail.com>
2020-07-31 18:01:41 +0900

    Face.py

---
+++ scripts/Face.py
@@ -0,0 +1,15 @@
+import glob as gb
+import cv2
+import matplotlib.pyplot as plt
+import os
+
+class Face:
+
+    def __init__(self):
+        pass
+
+    def extract_face(self):
+        pass
+
+    def get_face_image(self):
+        pass
