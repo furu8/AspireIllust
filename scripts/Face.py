6ad10e4d7877deffdc11a20f79205730a73ba8aa
furu8 <foooolurk1125@gmail.com>
2020-08-02 16:53:57 +0900

    Face.pyリファクタリング

--- scripts/Face.py
+++ scripts/Face.py
@@ -14,6 +14,8 @@ class Face:
         self.illustrator_account_path = ill_path
         # animeface
         self.cascade = cv2.CascadeClassifier('../info/feature_animeface/lbpcascade_animeface.xml')
+        # 顔画像リスト
+        self.face_img_list = []
 
     def triming_face(self, save_path):
         user_id_list = self.__get_user_id_list(self.ill_path)
@@ -33,11 +35,7 @@ class Face:
             # 保存するファイル名
             save_file_name = illust.split('/')[-1].split('.')[0]
             # 顔抽出
-            img = cv2.imread(illust, cv2.IMREAD_COLOR) # デフォルトカラー読み込み
-            print(img)
-            gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # グレースケール化
-            gray_img = cv2.equalizeHist(gray_img) # ヒストグラム平均化（見やすくなる）
-            face_list = self.cascade.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5, minSize=(24, 24))
+            img, face_list = self.__get_face_img(illust)
             
             if len(face_list) > 0:
                 # キャラごと
@@ -50,11 +48,13 @@ class Face:
                     user_path = save_path + str(user_id) + '/'
                     self.__make_directory(user_path)
                     # pngで保存
-                    cv2.imwrite(user_path + save_file_name + '_' + str(i) + '.png', face_img)
-                    print(save_file_name + '.png をトリミングし保存しました')
+                    path = user_path + save_file_name + '_' + str(i+1) + '.png'
+                    cv2.imwrite(path, face_img)
+                    print(path + 'をトリミングし保存しました')
+                    self.face_img_list.append(face_img)
                 
     def get_face_illust(self):
-        pass
+        return self.face_img_list
 
     # user情報取得
     def __get_user_id_list(self, path):
@@ -68,6 +68,19 @@ class Face:
             os.mkdir(path)
             print('{}にディレクトリ作成'.format(path))
 
-if __name__ == "__main__":
-    face = Face('../info/follow_user_account/user_account_using.json')
-    face.triming_face_from_illust('../data/pixiv/interim/face/face_test/', ['../data/pixiv/interim/face/face_test/test_kantoku.jpg'], 216403)
+    # 顔抽出
+    def __get_face_img(self, illust):
+        img = cv2.imread(illust, cv2.IMREAD_COLOR) # デフォルトカラー読み込み
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # グレースケール化
+        gray_img = cv2.equalizeHist(gray_img) # ヒストグラム平均化（見やすくなる）
+        face_list = self.cascade.detectMultiScale(gray_img, scaleFactor=1.01, minNeighbors=5, minSize=(24, 24)) # 見逃しを極力少なくパラメータ設定
+        return img, face_list
+
+# # テスト
+# if __name__ == "__main__":
+#     face = Face('../info/follow_user_account/user_account_using.json')
+#     # test1
+#     kantoku = 216403
+#     face.triming_face_from_illust('../data/pixiv/interim/face/face_test/', ['../data/pixiv/interim/face/face_test/test_kantoku.jpg'], kantoku)
+#     # test2
+#     print(face.get_face_illust())
