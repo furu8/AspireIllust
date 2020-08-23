aa24a2ec66610b932dfa9246d031cc00fb70f7e5
furu8 <foooolurk1125@gmail.com>
2020-08-24 00:38:01 +0900

    Infomation

--- scripts/Infomation.py
+++ scripts/Infomation.py
@@ -2,7 +2,7 @@ import json
 
 class Information:
 
-    def __init__(path):
+    def __init__(self, path):
         # pixivのアカウント情報を取得
         my_account_path = path
         with open(my_account_path) as json_file:
@@ -14,3 +14,11 @@ class Information:
         u_id = self.json_obj['user_id']
     
         return p_id, pw, u_id
+
+# テスト
+if __name__ == "__main__":
+    myaccount_path = '../info/my_account/my_account.json' # 自分のアカウント情報保存先
+    info = Information(myaccount_path)
+    p_id, pw, u_id = info.get_my_pixiv_account()
+    
+    print(p_id, pw, u_id)
