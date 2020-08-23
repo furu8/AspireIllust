add11e694651b501d314df8ac787065514850569
furu8 <foooolurk1125@gmail.com>
2020-08-24 02:07:48 +0900

    見やすく修正

--- scripts/Main.py
+++ scripts/Main.py
@@ -6,32 +6,52 @@ from UserRecord import UserRecord
 from DownloadIllust import DownloadIllust
 from Face import Face
 
-def main():
-    ## pixivのアカウント情報を取得
-    info = Information('../info/my_account/my_account.json')
+def pixiv_my_account_info():
+    my_account_path = '../info/my_account/my_account.json' # 自分のアカウント情報保存先
+    info = Information(my_account_path)
     p_id, pw, u_id = info.get_my_pixiv_account()
 
-    ## pixivログイン
-    api = Login()
-    api.pixiv_login(p_id, pw)
-    ## pixiv api
-    p_api, p_aapi = api.get_pixvi_api()
+    return p_id, pw, u_id    
 
-    ## ユーザ情報を保存
+def pixiv_user_account_info(u_id, p_aapi):
+    user_account_path = '../info/follow_user_account/user_account.json' # ユーザのアカウント情報保存先
     ur = UserRecord()
-    # 新しくフォローしたら作動
-    # ur.register_user_record(u_id, p_aapi)
-    # ur.post_user_record('../info/follow_user_account/user_account.json') 
+    ur.register_user_record(u_id, p_aapi) # 登録
+    ur.save_user_record(user_account_path) # 保存
 
-    ## ユーザのイラストを保存
-    using_illust_json_path = '../info/follow_user_account/user_account_using.json'
-    # 新しく作品があったら作動
-    # dl = DownloadIllust(p_api, p_aapi)
-    # dl.pixiv_download(using_illust_json_path)
+def pixiv_api(p_id, pw):
+    api = API()
+    api.pixiv_login(p_id, pw) # ログイン
+    p_api, p_aapi = api.get_pixvi_api() # api
+    
+    return p_api, p_aapi
 
-    ## イラストの顔を抽出
-    face = Face(using_illust_json_path)
-    face.triming_face('../data/pixiv/interim/face/')
+def pixiv_download(p_api, p_aapi):
+    save_path = '../data/pixiv/row/' # 保存先
+    dl = DownloadIllust(p_api, p_aapi)
+    dl.pixiv_download(save_path)
+
+def pixiv_triming_illust():
+    save_path = '../data/pixiv/interim/face/' # 保存先
+    face = Face()
+    face.triming_face(save_path)
+
+def main():
+    # pixivのアカウント情報を取得
+    p_id, pw, u_id = pixiv_my_account_info()
+
+    # pixiv api
+    p_api, p_aapi = pixiv_api(p_id, pw)
+
+    # ユーザ情報を登録・保存
+    pixiv_user_account_info(u_id, p_aapi)
+    
+    # ユーザのイラストを保存
+    pixiv_download(p_api, p_aapi)
+  
+    # イラストの顔を抽出
+    pixiv_triming_illust()
+    
 
 if __name__ == "__main__":
     main()
