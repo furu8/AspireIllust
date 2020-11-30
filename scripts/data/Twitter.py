46165b94d6853f9a9f77c9a3e41d8345100de686
furu8 <foooolurk1125@gmail.com>
2020-11-30 14:26:51 +0900

    Twitter引数実行

--- scripts/data/Twitter.py
+++ scripts/data/Twitter.py
@@ -10,10 +10,21 @@ class Twitter:
         self.ACCESS_TOKEN_SECRET = twitter_json['Access_token_secret']
 
         # twitter api
-        auth = tweepy.OAuthHandler(self.CONSUMER_KEY, self.CONSUMER_SECRET)
+        auth = tweepy.OAuthHandler(self.CONSUMER_KEY, self.CONSUMER_SECRET_KEY)
         auth.set_access_token(self.ACCESS_TOKEN, self.ACCESS_TOKEN_SECRET)
         self.twitter_api = tweepy.API(auth, wait_on_rate_limit = True)
 
         # UserRecordの保存先
         
         # ユーザのアカウント情報データフレーム
+
+        self.search_results = tweepy.Cursor(self.twitter_api.user_timeline, screen_name='8_furu8', include_rts=False).items(10)
+
+if __name__ == "__main__":
+    from Infomation import Information
+    info = Information()
+    twitter_json = info.get_twitter_json()
+
+    twitter = Twitter(twitter_json)
+    for result in twitter.search_results:
+        print(result.id)
