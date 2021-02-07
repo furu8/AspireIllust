0e9ba55163953b131cb1b5556d1fac76f6814674
furu8 <foooolurk1125@gmail.com>
2021-02-07 22:43:39 +0900

    fix:pathからユーザを読み込むのは引数指定したときのみに仕様変更

--- scripts/data/Pixiv.py
+++ scripts/data/Pixiv.py
@@ -18,58 +18,26 @@ class Pixiv:
         self.pixiv_api.login(self.P_ID, self.PW)
         self.pixiv_aapi.login(self.P_ID, self.PW)
 
-        # UserRecordの保存先
-        self.user_record_path = '../../info/follow_user_account/pixiv_user_account.json'
-
         # ユーザのアカウント情報データフレーム
         self.user_df = pd.DataFrame(columns=['user_id', 'user_name'])
 
-    # ユーザ情報取得
-    def get_user_record(self, path):
-        """
-        引数pathにはuser_account.jsonとuser_account_using.jsonを指定
-        """
-        df = pd.read_json(path)
-        return df
 
-    # 自分のフォロー欄の情報を記録
-    def register_user_record(self):
-        # フォローユーザ取得
-        follow_json = self.pixiv_aapi.user_following(self.U_ID) 
-        
-        # JSONデータをリストに記録 
-        user_id_list, user_name_list = self.__add_id_name(follow_json)
-
-        # 保存
-        self.__save_user(user_id_list, user_name_list)
-
-    def __add_id_name(self, follow_json):
-        user_id_list = []
-        user_name_list = []
-
-        for result in follow_json.user_previews:
-            user_id_list.append(result.user.id)
-            user_name_list.append(result.user.name)
-
-        return user_id_list, user_name_list
-
-    def __save_user(self, user_id_list, user_name_list):
-        self.user_df['user_id'] = user_id_list
-        self.user_df['user_name'] = user_name_list
-        self.user_df.to_json(self.user_record_path)
-
-        print(self.user_record_path + 'に保存しました')
-
-    def download_illustrator_illusts(self):
+    def download_illustrator_illusts(self, load_path=None):
         """
         記録してあるイラストレータの画像すべてをダウンロード
         """
         # ユーザ情報取得
-        using_illust_path = '../../info/follow_user_account/pixiv_user_account_using.json' 
-        user_df = self.get_user_record(using_illust_path)
-        
-        for user_id in user_df['user_id'].values:
-            self.download_illusts(user_id)
+        if not load_path is None:
+            # using_illust_path = '../../info/follow_user_account/pixiv_user_account_using.json' 
+            self.user_df = self.__get_user_record_path(load_path)
+        elif self.user_df.empty:
+            print('user_dfが空なため、register_user_recordメソッドを実行します')
+            self.register_user_record()
+
+        # ダウンロード実行
+        for user_id in self.user_df['user_id'].values:
+            self.download_illusts(user_id)            
+
 
     def download_illusts(self, user_id):
         """
@@ -95,6 +63,7 @@ class Pixiv:
                 time.sleep(1)
         print('\nfinish download\n')
     
+
     # ディレクトリがなければ作成
     def __make_directory(self, path):
         if not os.path.exists(path):
@@ -102,13 +71,50 @@ class Pixiv:
             print('{}にディレクトリ作成'.format(path))
 
 
+    # pathからユーザ情報取得
+    def __get_user_record_path(self, path):
+        """
+        引数pathにはuser_account.jsonとuser_account_using.jsonを指定
+        """
+        return pd.read_json(path)
+
+
+    # 自分のフォロー欄の情報を記録
+    def register_user_record(self, save_path=None):
+        # フォローユーザ取得
+        follow_json = self.pixiv_aapi.user_following(self.U_ID) 
+        
+        # JSONデータをリストに記録 
+        user_id_list, user_name_list = self.__add_id_name(follow_json)
+
+        # 登録
+        self.user_df['user_id'] = user_id_list
+        self.user_df['user_name'] = user_name_list
+
+        # 保存
+        # # UserRecordの保存先
+        # self.user_record_path = '../../info/follow_user_account/pixiv_user_account.json'
+        if not save_path is None:
+            self.user_df.to_json(self.user_record_path)
+            print(self.user_record_path + 'に保存しました')
+            
+
+    def __add_id_name(self, follow_json):
+        user_id_list = []
+        user_name_list = []
+
+        for result in follow_json.user_previews:
+            user_id_list.append(result.user.id)
+            user_name_list.append(result.user.name)
+
+        return user_id_list, user_name_list
+
+        
 if __name__ == "__main__":
     from Infomation import Information
     info = Information()
     pixiv_json = info.get_pixiv_json()
     pixiv = Pixiv(pixiv_json)
-    # pixiv.register_user_record()
-    # print(pixiv.get_user_record())
+    pixiv.register_user_record()
+    print(pixiv.user_df)
     pixiv.download_illustrator_illusts()
-
-
