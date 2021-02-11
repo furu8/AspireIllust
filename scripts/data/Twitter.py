bacbf980e0b50d4257c97ada3f52c2b0c42873c2
furu8 <foooolurk1125@gmail.com>
2021-02-11 23:07:59 +0900

    feat:画像URLのみ取得するメソッド定義

--- scripts/data/Twitter.py
+++ scripts/data/Twitter.py
@@ -20,61 +20,84 @@ class Twitter:
         self.user_df = pd.DataFrame(columns=['user_id', 'user_name'])
 
 
-    # def download_illustrator_illusts(self, load_path=None):
-    #     """
-    #     記録してあるイラストレータの画像すべてをダウンロード
-    #     """
-    #     # ユーザ情報取得
-    #     if not load_path is None:
-    #         # using_illust_path = '../../info/follow_user_account/pixiv_user_account_using.json' 
-    #         self.user_df = self.__get_user_record_path(load_path)
-    #     elif self.user_df.empty:
-    #         print('user_dfが空なため、register_user_recordメソッドを実行します')
-    #         self.register_user_record()
+    def download_illustrator_illusts(self, load_path=None):
+        """
+        記録してあるイラストレータの画像すべてをダウンロード
+        """
+        # ユーザ情報取得
+        if not load_path is None:
+            # using_illust_path = '../../info/follow_user_account/pixiv_user_account_using.json' 
+            self.user_df = self.__get_user_record_path(load_path)
+        elif self.user_df.empty:
+            print('user_dfが空なため、register_user_recordメソッドを実行します')
+            self.register_user_record()
 
-    #     # ダウンロード実行
-    #     for user_id in self.user_df['user_id'].values:
-    #         self.download_illusts(user_id)            
+        # ユーザごとにダウンロード実行
+        for user_id in self.user_df['user_id'].values:
+            # ユーザを指定し、JSON取得
+            res = tweepy.Cursor(self.api.user_timeline, user_id=user_id).items(5)
+            url_list = self.search_illust(res)
+            self.download_illusts(url_list)
+                  
+                  
+    def search_illust(res):
+        url_list = []
+
+        tweet_list = res['statuses']
+        for tweet in tweet_list:
+            if 'extended_entities' in tweet:
+                for media in tweet['extended_entities']['media']:
+                    media_type = media['type']
+                    url = media['media_url']
+                    if media_type == 'photo':
+                        url_list.append(url)
+        return url_list
 
 
-    # def download_illusts(self, user_id):
-    #     """
-    #     引数のIDのイラストレータの画像をすべてダウンロード
-    #     """
-    #     # ユーザを指定し、JSON取得
-    #     works_info = self.pixiv_api.users_works(user_id, per_page=300)
+    def download_illusts(self, user_id):
+        """
+        引数のIDのイラストレータの画像をすべてダウンロード
+        """
+        
 
-    #     # ユーザのディレクトリがなければ作成
-    #     save_path = '../../data/pixiv/row/' + str(user_id) + '/'
-    #     self.__make_directory(save_path)
+        # ユーザのディレクトリがなければ作成
+        save_path = '../../data/twitter/row/' + str(user_id) + '/'
+        self.__make_directory(save_path)
 
-    #     # ダウンロード
-    #     print('start download\n')
-    #     for work_info in works_info.response:
-    #         # DL済みの画像かどうか判定
-    #         if not os.path.exists(save_path + work_info.image_urls.large.split('/')[-1]):
-    #             # タイトル出力
-    #             print(work_info.title.replace("/", "-")) # '/'はPathとして扱われるため回避
-    #             # 保存
-    #             self.pixiv_aapi.download(work_info.image_urls.large, path=save_path)
-    #             # マナー
-    #             time.sleep(1)
-    #     print('\nfinish download\n')
+        # ダウンロード
+        print('start download\n')
+        for work_info in works_info:
+            if 'extended_entities' in work_info:
+            for media in work_info['extended_entities']['media']:
+                media_type = media['type']
+                url = media['media_url']
+                if media_type == 'photo':
+                    url_list.append(url)
+            
+            # DL済みの画像かどうか判定
+            # if not os.path.exists(save_path + work_info.image_urls.large.split('/')[-1]):
+                # タイトル出力
+                # print(work_info.title.replace("/", "-")) # '/'はPathとして扱われるため回避
+                # 保存
+                # self.pixiv_aapi.download(work_info.image_urls.large, path=save_path)
+                # マナー
+                # time.sleep(1)
+        print('\nfinish download\n')
     
 
-    # # ディレクトリがなければ作成
-    # def __make_directory(self, path):
-    #     if not os.path.exists(path):
-    #         os.mkdir(path)
-    #         print('{}にディレクトリ作成'.format(path))
+    # ディレクトリがなければ作成
+    def __make_directory(self, path):
+        if not os.path.exists(path):
+            os.mkdir(path)
+            print('{}にディレクトリ作成'.format(path))
 
 
-    # # pathからユーザ情報取得
-    # def __get_user_record_path(self, path):
-    #     """
-    #     引数pathにはuser_account.jsonとuser_account_using.jsonを指定
-    #     """
-    #     return pd.read_json(path)
+    # pathからユーザ情報取得
+    def __get_user_record_path(self, path):
+        """
+        引数pathにはuser_account.jsonとuser_account_using.jsonを指定
+        """
+        return pd.read_json(path)
 
 
     # 自分のフォロー欄の情報を記録
@@ -115,6 +138,7 @@ if __name__ == "__main__":
 
     twitter = Twitter(twitter_json)
     # twitter.register_user_record('../../info/follow_user_account/twitter_user_account.json')
-    twitter.register_user_record()
-    print(twitter.user_df)
-    
+    # twitter.register_user_record()
+    # print(twitter.user_df)
+    twitter.download_illusts(159060598)
+        
